3816e7e09de079361569ca1d46071851592fb62a
Olexandr004 <romanyyk2004@gamil.com>
2024-04-29 13:20:07 +0300

    add first project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <Header />
+            <div>
+                <Sidebar />
+                <MainContainer />
+            </div>
+        </div>
+    );
 }
 
-export default App
+function Header() {
+    return (
+        <header>
+            <h1>Fake ROZETKA</h1>
+        </header>
+    );
+}
+
+function Sidebar() {
+    return (
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="/">Телефони</a></li>
+                    <li><a href="/">Ноутбуки</a></li>
+                    <li><a href="/">Планшети</a></li>
+                    <li><a href="/">Компьютери</a></li>
+                    <li><a href="/">Телевізори</a></li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
+
+function MainContainer() {
+    return (
+        <main>
+            <div>
+                <p>Натисни на одну з категорій, щоб подивитися товар!</p>
+            </div>
+        </main>
+    )
+}
+
+export default App;
